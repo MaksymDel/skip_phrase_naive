4b745add04ad3e7c67e81d0aca2fbb86f519d64b
Max Del <max.del.edu@gmail.com>
2017-12-25 13:42:54 +0200

    Add skipgram dataset  builder

--- scripts/util.py
+++ scripts/util.py
@@ -1,11 +1,57 @@
-def build_dataset_skip_gram(path_to_data):
-    with open(path_to_data, 'r') as f:
-        lines = f.readlines()
+def build_dataset_skip_gram(path_to_in, path_to_out, window_size=2):
+    with open(path_to_in, 'r') as fin, open(path_to_out, 'a+') as fout:
+        print("Start processing file %s...", path_to_in)
+        lines = fin.readlines()
+        result_lines = []       
+        for line in lines:
+            examples = process_line_skip_gram(line)
+            examples = [" ".join(ex) for ex in examples]
+            result_lines.extend(examples)
 
-    return lines
+        print(result_lines)
+
+        fout.writelines("\n".join(result_lines))
+        print("Skip-gram dataset is wrote to the %s.", path_to_out)
+
+def process_line_skip_gram(line, window_size=2):
+    words = line.split()
+
+    if len(words) < 2:
+        return None
+    
+    skip_gram_examples = []
+    for i in range(len(words)):
+        try:
+            for j in reversed(range(window_size)):
+                j += 1 # to make window index start at 1
+                if (i - j) < 0:
+                    continue
+                skip_gram_examples.append((words[i], words[i-j]))  
+
+            for j in range(window_size):
+                j += 1 # to make window index start at 1
+                skip_gram_examples.append((words[i], words[i+j]))
+        except IndexError:
+            pass
+
+    return skip_gram_examples
+
 
 def build_dataset_skip_phrase(path_to_data):
     with open(path_to_data, 'r') as f:
         lines = f.readlines()
 
     return lines
+
+"""
+def build_dataset_skip_gram(path_to_in, path_to_out, window_size=2):
+    with open(path_to_in, 'r') as fin, open(path_to_out, 'a') as fout:
+        print("Start processing file %s...", path_to_in)
+        line = fin.readline()
+        while line:
+            examples = process_line_skip_gram(line)
+            for ex in examples:
+                fout.writelines(" ".join(ex))
+                fout.write('\n')
+        print("Skip-gram dataset is wrote to the %s.", path_to_out)
+"""
--- tests/scripts/util_test.py
+++ tests/scripts/util_test.py
@@ -1,12 +1,73 @@
+import os
 from allennlp.common.testing import AllenNlpTestCase
-from scripts.util import build_dataset_skip_gram, build_dataset_skip_phrase
+
+from scripts.util import *
 
 class TestDataUtil(AllenNlpTestCase):
     def test_build_dataset_skip_gram(self):
-        skipgram_data = build_dataset_skip_gram('tests/fixtures/lines.en')
-        assert len(skipgram_data) == 4
-        assert skipgram_data[3].split()[-2] == 'disgusting'  
+        out_path = 'tests/fixtures/lines-skipgram.en'
+        try:
+            os.remove(out_path)
+        except OSError:
+            pass
+        build_dataset_skip_gram('tests/fixtures/lines.en', out_path, 2)
+        with open(out_path, 'r') as f:
+            examples = f.readlines()
+            assert examples[-1] == '! disgusting'
+
+    def test_process_line_skip_gram(self):
+        l = "Jack of all trades , master of none ."
+        training_examples = process_line_skip_gram(l, 2)
+        
+        assert len(training_examples) == 30
+        assert training_examples[0] == ('Jack', 'of')
+        assert training_examples[1] == ('Jack', 'all')
+
+        assert training_examples[2] == ('of', 'Jack')
+        assert training_examples[3] == ('of', 'all')
+        assert training_examples[4] == ('of', 'trades')
+
+        assert training_examples[5] == ('all', 'Jack')
+        assert training_examples[6] == ('all', 'of')
+        assert training_examples[7] == ('all', 'trades')
+        assert training_examples[8] == ('all', ',')
+        
+        assert training_examples[9] == ('trades', 'of')
+        assert training_examples[10] == ('trades', 'all')
+        assert training_examples[11] == ('trades', ',')
+        assert training_examples[12] == ('trades', 'master')
+
+        assert training_examples[13] == (',', 'all')
+        assert training_examples[14] == (',', 'trades')
+        assert training_examples[15] == (',', 'master')
+        assert training_examples[16] == (',', 'of')
+
+        assert training_examples[17] == ('master', 'trades')
+        assert training_examples[18] == ('master', ',')
+        assert training_examples[19] == ('master', 'of')
+        assert training_examples[20] == ('master', 'none')
+        
+        assert training_examples[21] == ('of', ',')
+        assert training_examples[22] == ('of', 'master')
+        assert training_examples[23] == ('of', 'none')
+        assert training_examples[24] == ('of', '.')
+
+        assert training_examples[25] == ('none', 'master')
+        assert training_examples[26] == ('none', 'of')
+        assert training_examples[27] == ('none', '.')
+
+        assert training_examples[28] == ('.', 'of')
+        assert training_examples[29] == ('.', 'none')
+
+        l = "you like"
+        training_examples = process_line_skip_gram(l, 2)
+        assert training_examples[0] == ('you', 'like')
+        assert training_examples[1] == ('like', 'you')
+
+        l = "try"
+        training_examples = process_line_skip_gram(l, 2)
+        assert training_examples == None 
 
     def test_build_dataset_skip_phrase(self):
         skipphrase_data = build_dataset_skip_phrase('tests/fixtures/lines.en')
-        assert len(skipphrase_data) == 4
+        pass
